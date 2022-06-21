11e4318db7f224507a812c226bf2c0d79357f79a
Eduard-Developer <91020951+Eduard-Developer@users.noreply.github.com>
2022-06-21 22:35:24 +0200

    "Daily" Updates

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ num = 3
 integer = 1
 
 
-alpthabet = string.ascii_lowercase
+donotuse_alpthabet = string.ascii_lowercase
 
-print("the alphabet is working and it is: ", str(alpthabet))
-if str(alpthabet[1]) == "a":
+print("the alphabet is working and it is: ", str(donotuse_alpthabet))
+if str(donotuse_alpthabet[1]) == "a":
     print("the first letter is a so it should be fine")
 
 if input("Type Hey to start \n") == "Hey":
@@ -35,6 +35,8 @@ if input("Do you want to Add Tasks to your List? (Y) ") == "Y":
     print("Add Your Task HERE: ")
     _tasksTodo = input()
 
+_tasksTodo = ""
+
 
 def func():
     outfile = open("app.txt", "a")
@@ -45,6 +47,15 @@ def func():
 
 def storepassword():
     print("TODO")
+
+def rotor(text, shift, alphabets):
+
+    def shift_Alphabet(alphabet):
+
+        return alphabet[shift:] + alphabet [:shift]
+
+    shifted_Alphabets = tuple(map(shift_Alphabet, alphabets))
+    final_alphabet = "".join() 
 
 time.sleep(0.5)
 if input("Do you want to set up a Password (Y/N)") == "Y":
